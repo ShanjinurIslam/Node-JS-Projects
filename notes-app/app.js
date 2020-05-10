d1a874cd99d9e8f87c711614cc2a03553fa91f37
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-05-10 10:54:43 +0600

    npm modules add

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,6 @@
+const validator = require('validator')
 const getNotes = require('./notes')
 
 console.log(getNotes())
+console.log(validator.isEmail('spondoncsebuet@gmail.com'))
+console.log(validator.isURL('https://www.google.com'))
