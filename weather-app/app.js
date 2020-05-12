ff770e004d8e33d37957701221462bd7398854d5
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-05-12 11:15:08 +0600

    weather app

---
+++ weather-app/app.js
@@ -0,0 +1,51 @@
+const request = require('request')
+
+const address = process.argv[2]
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2hhbmppbnVyIiwiYSI6ImNrYTJxNWxmZDA1dW4zbWxtZ3Bhc2tyNG0ifQ.iJraz1iVL4TBJq0Lyt3ZrA&limit=1';
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback(error, undefined)
+        }
+        else {
+            const data = {
+                place_name: response.body.features[0].place_name,
+                lat: response.body.features[0].center[1],
+                long: response.body.features[0].center[0],
+            }
+            callback(error, data)
+        }
+    })
+}
+
+const weather = (lat, long, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=616fde5ce60424825a81e3f75f97139a&query=' + encodeURIComponent(lat) + ',' + encodeURIComponent(long)
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback(error, undefined)
+        }
+        else {
+            callback(undefined, response.body)
+        }
+    })
+}
+
+
+geocode(address, (error, data) => {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        weather(data.lat, data.long, (error, data) => {
+            if (error) {
+
+            }
+            else {
+                console.log(data.current)
+            }
+        })
+    }
+})
